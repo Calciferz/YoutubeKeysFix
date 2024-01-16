5436dbcb90dc739c9d726cee81d58fc38d7f833f
Calcifer <5092597+Calciferz@users.noreply.github.com>
2024-01-17 00:27:44 +0100

    Version: 2.0.0

--- YoutubeKeysFix.user.js
+++ YoutubeKeysFix.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YouTube arrow keys FIX
-// @version      1.3.1
+// @version      2.0.0
 // @description  Fix YouTube keyboard controls (arrow keys) to be more consistent (Left,Right - jump, Up,Down - volume) after page load or clicking individual controls.
 // @author       Calcifer
 // @license      MIT
